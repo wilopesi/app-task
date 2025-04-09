a1d0cf7489ddfa2c0d08f3b8600e626a2f7c6d4d
W_Lopes <W_Lopes@Dell.com>
2025-04-09 15:29:17 -0300

    app task  finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    HeaderComponent,
+    UserComponent,
+    TasksComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app-task';
+   users = DUMMY_USERS;
+   selectedUserId?: string;
+
+   get selectedUser(){
+    return this.users.find((user) => user.id === this.selectedUserId);
+   }
+
+   onSelectUser(id: string){
+      this.selectedUserId = id;
+   }
 }
